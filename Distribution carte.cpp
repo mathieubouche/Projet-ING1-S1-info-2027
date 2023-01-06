333fbc1a97d3fb67b9c1e8dd91e04134afc8a581
Penginsan168 <64909107+Penginsan168@users.noreply.github.com>
2023-01-06 09:13:02 +0100

    Update Distribution carte.cpp

--- Distribution carte.cpp
+++ Distribution carte.cpp
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NUM_CARDS 24
-#define MAX_PLAYERS 4
+#define NOM_CARTES 24
+#define MAX_JOUEURS 4
 
-char cartes[NUM_CARDS] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
-char mains[MAX_PLAYERS][NUM_CARDS / 2];
+char cartes[NOM_CARTES] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
+char mains[MAX_JOUEURS][NOM_CARTES / 2];
 
 void melangeCartes(char *cartes) {
-    for (int i = 0; i < NUM_CARDS; i++) {
-        int j = rand() % NUM_CARDS;
+    for (int i = 0; i < NOM_CARTES; i++) {
+        int j = rand() % NOM_CARTES;
         char temp = cartes[i];
         cartes[i] = cartes[j];
         cartes[j] = temp;
     }
 }
 
-void distributionCartes(char *cartes, int nbJoueur, char mains[][NUM_CARDS / 2]) {
-    int cartesParJoueur = NUM_CARDS / nbJoueur;
+void distributionCartes(char *cartes, int nbJoueur, char mains[][NOM_CARTES / 2]) {
+    int cartesParJoueurs = NOM_CARTES / nbJoueur;
     for (int i = 0; i < nbJoueur; i++) {
-        for (int j = 0; j < cartesParJoueur; j++) {
-            mains[i][j] = cartes[i * cartesParJoueur + j];
+        for (int j = 0; j < cartesParJoueurs; j++) {
+            mains[i][j] = cartes[i * cartesParJoueurs + j];
         }
     }
 }
 
 int main() {
     srand(time(NULL));
-    //A adapter avec les autres programmes
+    
     int nbJoueur;
     printf("Nombre de joueurs (2-4): ");
     scanf("%d", &nbJoueur);
@@ -37,10 +37,10 @@ int main() {
 
     distributionCartes(cartes, nbJoueur, mains);
 
-    //Retour des cartes
+    // Print les mains
     for (int i = 0; i < nbJoueur; i++) {
         printf("Main du joueur %d: ", i + 1);
-        for (int j = 0; j < NUM_CARDS / nbJoueur; j++) {
+        for (int j = 0; j < NOM_CARTES / nbJoueur; j++) {
             printf("%c ", mains[i][j]);
         }
         printf("\n");
